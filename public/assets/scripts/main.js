b3d8e7a6ff48fda8cf1e4220e8e93473d6f085d1
axis <asebastian.martinf@gmail.com>
2026-09-19 00:19:10 -0500

    feat(landing): add function to flatten nested translation objects

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -2,3 +2,16 @@
 
 let currentLang = localStorage.getItem('pulsepower-lang') || 'es';
 
+// Flatten nested object: { "nav": { "home": "Inicio" } } -> { "nav.home": "Inicio" }
+function flattenTranslations(obj, prefix) {
+    prefix = prefix || '';
+    return Object.keys(obj).reduce(function (acc, key) {
+        var fullKey = prefix ? prefix + '.' + key : key;
+        if (obj[key] !== null && typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+            Object.assign(acc, flattenTranslations(obj[key], fullKey));
+        } else {
+            acc[fullKey] = obj[key];
+        }
+        return acc;
+    }, {});
+}
