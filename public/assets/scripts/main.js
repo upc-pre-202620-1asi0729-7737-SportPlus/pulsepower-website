4f1390c0f2b996cbe7f5d13f13fb5571480871e0
axis <asebastian.martinf@gmail.com>
2026-09-19 00:22:15 -0500

    feat(landing): add scroll event to change header background on scroll

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -137,3 +137,13 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
+    // Header background on scroll
+    var header = document.querySelector('.header');
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 40) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
+
